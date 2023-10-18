79a034bb53c1ee6a334eae741e2541a3f515c797
Candice Ariel <candicewangsa@gmail.com>
2023-10-18 16:04:25 +0700

    styling

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ $(document).ready(function(){
   var y = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
   
   $(".frame__container")[0].style.height = y-106 + "px";
-  $(".slider__container")[0].style.height = y-290 + "px";
+  $(".slider__container")[0].style.height = y-390 + "px";
+  $(".slider__container")[0].style.width = x-100 + "px";
+  $(".slider-images__container--back")[0].style.height = y-424 + "px";
   $(".slider-images__container--front")[0].style.height = y-300 + "px";
 
   // $(".slider-images__container--back")[0].style.top = "-" + y - 0.1 + "%";
